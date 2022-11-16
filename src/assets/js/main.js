87337e981afe027dd8ea8d71ed6c55fd1bab1f68
denys_kosyriev <utross2241@gmail.com>
2022-11-16 06:29:46 +0200

    3(updating all code)

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log(1)
+
+  // new Swiper('.intro-swiper', {
+  //   loop: true,
+  //   autoplay: {
+  //    // delay: 4000,
+  //   },
+  //  // simulateTouch: false,
+  //   speed: 800,
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //   },
+  // });
 });
+
